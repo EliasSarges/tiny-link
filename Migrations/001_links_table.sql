18c4ebf4c32123c1adb974d6712c1ad0a0558e03
Elias Castro <38994457+EliasSarges@users.noreply.github.com>
2024-04-13 12:08:25 -0300

    feat: add migration runner

---
+++ Migrations/001_links_table.sql
@@ -0,0 +1,7 @@
+create table
+  if not exists LINKS (
+    ID string primary key,
+    URL text not null,
+    CLICKS text not null,
+    CREATED_AT timestamp not null
+  );
---
+++ Migrations/MigrationRunner.cs
@@ -0,0 +1,24 @@
+using Microsoft.Data.Sqlite;
+
+public static class MigrationRunner
+{
+  public static void RunMigrations()
+  {
+    using var connection = new DatabaseManager();
+    {
+      IOrderedEnumerable<string> migrationFiles = Directory.GetFiles("Migrations", "*.sql").OrderBy(name => name);
+
+      foreach (var migration in migrationFiles)
+      {
+        string sql = File.ReadAllText(migration);
+
+        SqliteCommand command = connection.CreateCommand();
+        command.CommandText = sql;
+
+        command.ExecuteNonQuery();
+      }
+    }
+
+    Console.Write(connection);
+  }
+}
